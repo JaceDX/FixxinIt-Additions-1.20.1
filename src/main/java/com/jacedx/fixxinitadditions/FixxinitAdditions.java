51ab3da122d42118e1396f41da12335631b75789
Jace Hong <jacetylerhong@gmail.com>
2024-06-07 18:08:24 -0400

    Some blocks and raw blocks.

--- src/main/java/com/jacedx/fixxinitadditions/FixxinitAdditions.java
+++ src/main/java/com/jacedx/fixxinitadditions/FixxinitAdditions.java
@@ -1,5 +1,6 @@
-package com.jacedx.fixxinitadditions;
+package com.jacedx.fixxinitadditions;   // i don't like making the stupid textures for the stupid items and blocks ;(
 
+import com.jacedx.fixxinitadditions.block.ModBlocks;
 import com.jacedx.fixxinitadditions.item.ModItems;
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.item.CreativeModeTabs;
@@ -14,11 +15,9 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 @Mod(FixxinitAdditions.MOD_ID)
-public class FixxinitAdditions
-{
+public class FixxinitAdditions {
 
     public static final String MOD_ID = "fixxinit_additions";
-
 
 
     private static final Logger LOGGER = LogUtils.getLogger();
@@ -27,6 +26,7 @@ public class FixxinitAdditions
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
@@ -35,12 +35,13 @@ public class FixxinitAdditions
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
+
     private void commonSetup(final FMLCommonSetupEvent event) {
 
     }
 
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.FIXXALDRITE_INGOT);
             event.accept(ModItems.FIXXANDRITE_INGOT);
             event.accept(ModItems.FIXXNUNDRITE_INGOT);
@@ -55,5 +56,21 @@ public class FixxinitAdditions
             event.accept(ModItems.RAW_FIXXUNDRITE);
             event.accept(ModItems.RAW_FIXXUNMIUM);
         }
+            if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+                event.accept(ModBlocks.FIXXALDRITE_BLOCK);
+                event.accept(ModBlocks.FIXXANDRITE_BLOCK);
+                event.accept(ModBlocks.FIXXNUNDRITE_BLOCK);
+                event.accept(ModBlocks.FIXXSTUDRITE_BLOCK);
+                event.accept(ModBlocks.FIXXUNDRITE_BLOCK);
+                event.accept(ModBlocks.FIXXUNMIUM_BLOCK);
+
+                event.accept(ModBlocks.RAW_FIXXALDRITE_BLOCK);
+                event.accept(ModBlocks.RAW_FIXXANDRITE_BLOCK);
+                event.accept(ModBlocks.RAW_FIXXNUNDRITE_BLOCK);
+                event.accept(ModBlocks.RAW_FIXXSTUDRITE_BLOCK);
+                event.accept(ModBlocks.RAW_FIXXUNDRITE_BLOCK);
+                event.accept(ModBlocks.RAW_FIXXUNMIUM_BLOCK);
+            }
+        }
     }
-}
+
---
+++ src/main/java/com/jacedx/fixxinitadditions/block/ModBlocks.java
@@ -0,0 +1,77 @@
+package com.jacedx.fixxinitadditions.block;
+
+import com.jacedx.fixxinitadditions.FixxinitAdditions;
+import com.jacedx.fixxinitadditions.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, FixxinitAdditions.MOD_ID);
+
+    public static final RegistryObject<Block> FIXXALDRITE_BLOCK = registerBlock("fixxaldrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> FIXXANDRITE_BLOCK = registerBlock("fixxandrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> FIXXNUNDRITE_BLOCK = registerBlock("fixxnundrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> FIXXSTUDRITE_BLOCK = registerBlock("fixxstudrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+        public static final RegistryObject<Block> FIXXUNDRITE_BLOCK = registerBlock("fixxundrite_block",
+                () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                        .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> FIXXUNMIUM_BLOCK = registerBlock("fixxunmium_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+
+    public static final RegistryObject<Block> RAW_FIXXALDRITE_BLOCK = registerBlock("raw_fixxaldrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> RAW_FIXXANDRITE_BLOCK = registerBlock("raw_fixxandrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> RAW_FIXXNUNDRITE_BLOCK = registerBlock("raw_fixxnundrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> RAW_FIXXSTUDRITE_BLOCK = registerBlock("raw_fixxstudrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> RAW_FIXXUNDRITE_BLOCK = registerBlock("raw_fixxundrite_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+    public static final RegistryObject<Block> RAW_FIXXUNMIUM_BLOCK = registerBlock("raw_fixxunmium_block",
+            () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.DIAMOND)
+                    .requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
+
+
+
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
+        return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus) {
+        BLOCKS.register(eventBus);
+    }
+}
