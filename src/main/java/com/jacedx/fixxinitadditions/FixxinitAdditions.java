3dcd2157d1ddd3e783a5afe0d511923b7442e673
Jace Hong <jacetylerhong@gmail.com>
2024-06-07 16:24:56 -0400

    Some new ingots and raw versions

--- src/main/java/com/jacedx/fixxinitadditions/FixxinitAdditions.java
+++ src/main/java/com/jacedx/fixxinitadditions/FixxinitAdditions.java
@@ -1,7 +1,10 @@
 package com.jacedx.fixxinitadditions;
 
+import com.jacedx.fixxinitadditions.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -16,21 +19,41 @@ public class FixxinitAdditions
 
     public static final String MOD_ID = "fixxinit_additions";
 
+
+
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public FixxinitAdditions() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
-
+        modEventBus.addListener(this::addCreative);
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
-
     private void commonSetup(final FMLCommonSetupEvent event) {
 
+    }
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.FIXXALDRITE_INGOT);
+            event.accept(ModItems.FIXXANDRITE_INGOT);
+            event.accept(ModItems.FIXXNUNDRITE_INGOT);
+            event.accept(ModItems.FIXXSTUDRITE_INGOT);
+            event.accept(ModItems.FIXXUNDRITE_INGOT);
+            event.accept(ModItems.FIXXUNMIUM_INGOT);
+
+            event.accept(ModItems.RAW_FIXXALDRITE);
+            event.accept(ModItems.RAW_FIXXANDRITE);
+            event.accept(ModItems.RAW_FIXXNUNDRITE);
+            event.accept(ModItems.RAW_FIXXSTUDRITE);
+            event.accept(ModItems.RAW_FIXXUNDRITE);
+            event.accept(ModItems.RAW_FIXXUNMIUM);
+        }
     }
 }
---
+++ src/main/java/com/jacedx/fixxinitadditions/item/ModItems.java
@@ -0,0 +1,44 @@
+package com.jacedx.fixxinitadditions.item;
+
+import com.jacedx.fixxinitadditions.FixxinitAdditions;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FixxinitAdditions.MOD_ID);
+
+    public static final RegistryObject<Item> FIXXANDRITE_INGOT = ITEMS.register("fixxandrite_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FIXXUNDRITE_INGOT = ITEMS.register("fixxundrite_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FIXXALDRITE_INGOT = ITEMS.register("fixxaldrite_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FIXXSTUDRITE_INGOT = ITEMS.register("fixxstudrite_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FIXXNUNDRITE_INGOT = ITEMS.register("fixxnundrite_ingot",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> FIXXUNMIUM_INGOT = ITEMS.register("fixxunmium_ingot",
+            () -> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> RAW_FIXXANDRITE = ITEMS.register("raw_fixxandrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_FIXXUNDRITE = ITEMS.register("raw_fixxundrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_FIXXALDRITE = ITEMS.register("raw_fixxaldrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_FIXXSTUDRITE = ITEMS.register("raw_fixxstudrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_FIXXNUNDRITE = ITEMS.register("raw_fixxnundrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_FIXXUNMIUM = ITEMS.register("raw_fixxunmium",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
